7fd3f60799f0257d9a147aa4949c995633f44274
Nathan Leung <nleung@amazon.com>
2017-04-06 09:55:44 -0700

    Add forms page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(function(){
   $(".header").load("header.html");
   $(".footer").load("footer.html");
+
+  var url = window.location;
+  $('.navbar .nav').find('.active').removeClass('active');
+  $('.navbar .nav li a').each(function () {
+      if (this.href == url) {
+          $(this).parent().addClass('active');
+      }
+  });
 });
